c2b68ad7caccffe04efd845538dd40aae165d4fb
Eddy Nguyen <ch@eddeee888.me>
2023-02-26 16:08:39 +1100

    Do not mask errors as it's better to demo

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ export type ResolverContext = { data: typeof data };
 const yoga = createYoga<ResolverContext>({
   schema: createSchema<ResolverContext>({ typeDefs, resolvers }),
   context: { data },
+  maskedErrors: false,
 });
 const server = createServer(yoga);
 server.listen(3000, () => console.log("It's at http://localhost:3000/graphql"));

fa330a4aa864c1321af619b2da7246abc93905bf
Eddy Nguyen <ch@eddeee888.me>
2023-03-15 21:28:17 +1100

    Fix typing

--- src/schema/user/resolvers/User.ts
+++ src/schema/user/resolvers/User.ts
@@ -1,11 +1,12 @@
-import type { Book, UserResolvers } from "./../../types.generated";
+import type { BookMapper } from "src/schema/book/schema.mappers";
+import type { UserResolvers } from "./../../types.generated";
 export const User: UserResolvers = {
   /* Implement User resolver logic here */
   booksRead: (parent, __, { data }) => {
-    const books = Object.values(data.users_read_books).reduce<Book[]>(
+    const books = Object.values(data.users_read_books).reduce<BookMapper[]>(
       (res, [userId, bookId]) => {
         if (userId === parent.id) {
-          res.push({ ...data.books[bookId], __typename: "Book" });
+          res.push({ ...data.books[bookId] });
         }
         return res;
       },

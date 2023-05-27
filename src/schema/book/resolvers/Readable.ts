33e8b955a69958e76e623fc198013b9b91771d65
Eddy Nguyen <ch@eddeee888.me>
2023-05-27 17:04:54 +1000

    Bump server preset v0.5.0 (#13)
    * Bump preset versions
    
    * Regen with v0.5

--- src/schema/book/resolvers/Readable.ts
+++ src/schema/book/resolvers/Readable.ts
@@ -1,4 +1,4 @@
 import type { ReadableResolvers } from "./../../types.generated";
 export const Readable: ReadableResolvers = {
-  __resolveType: (parent) => parent.__typename,
+  /* Implement Readable union logic here */
 };

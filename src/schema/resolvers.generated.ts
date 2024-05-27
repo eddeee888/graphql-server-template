d69465b1509fde0520300a63f07359048617740e
Eddy Nguyen <github@eddeee888.me>
2024-05-27 23:20:34 +1000

    Remove unused types because of minimal

--- src/schema/resolvers.generated.ts
+++ src/schema/resolvers.generated.ts
@@ -5,12 +5,6 @@ import { character as Query_character } from "./character/resolvers/Query/charac
 import { readable as Query_readable } from "./book/resolvers/Query/readable";
 import { user as Query_user } from "./user/resolvers/Query/user";
 import { Book } from "./book/resolvers/Book";
-import { BookResult } from "./book/resolvers/BookResult";
-import { ExtraCharacter } from "./character/resolvers/ExtraCharacter";
-import { Fighter } from "./character/resolvers/Fighter";
-import { Magazine } from "./book/resolvers/Magazine";
-import { PayloadError } from "./base/resolvers/PayloadError";
-import { ShortNovel } from "./book/resolvers/ShortNovel";
 import { User as book_User } from "./book/resolvers/User";
 import { User as user_User } from "./user/resolvers/User";
 import { Wizard } from "./character/resolvers/Wizard";
@@ -24,12 +18,6 @@ export const resolvers: Resolvers = {
   },
 
   Book: Book,
-  BookResult: BookResult,
-  ExtraCharacter: ExtraCharacter,
-  Fighter: Fighter,
-  Magazine: Magazine,
-  PayloadError: PayloadError,
-  ShortNovel: ShortNovel,
   User: { ...book_User, ...user_User },
   Wizard: Wizard,
   DateTime: DateTimeResolver,

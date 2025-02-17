2d591202d946277cd0a2f243529101ec5f7d4953
Eddy Nguyen <ch@eddeee888.me>
2025-02-17 23:01:47 +1100

    Add CI (#89)
    * Add CI
    
    * Add missing types
    
    * Fix failing test

---
+++ bin/codegen-check.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo -e "\n=> Running codegen and checking diff..."
+
+yarn graphql-codegen
+
+echo "=> Check if commited files matches generated sample"
+if [[ `git status --porcelain` ]]; then
+  echo -e "x> Error!\n"
+  echo "=> Following files do not have expected result:"
+  git --no-pager diff HEAD --color
+  git ls-files --others --exclude-standard
+  echo -e "\n=> Have you tried running 'yarn graphql-codegen'?"
+  exit 1
+fi
+
+echo "=> Done checking codegen"

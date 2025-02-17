52e09ea079a0eb3091cfcd1997aa9f32e9221054
Eddy Nguyen <github@eddeee888.me>
2025-02-17 23:17:34 +1100

    Update app config

--- graphql.config.ts
+++ graphql.config.ts
@@ -2,6 +2,7 @@ import type { IGraphQLConfig } from "graphql-config";
 
 const config: IGraphQLConfig = {
   schema: "src/schema/**/*.graphql",
+  documents: "src/**/*.spec.ts",
 };
 
 export default config;

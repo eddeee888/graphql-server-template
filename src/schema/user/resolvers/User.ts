addc3ec462c954160f172c3a5d41f4fa6c83009c
Eddy Nguyen <ch@eddeee888.me>
2025-10-18 16:59:19 +1100

    Bump codegen packages (#121)
    * Bump codegen packages
    
    * Update types

--- src/schema/user/resolvers/User.ts
+++ src/schema/user/resolvers/User.ts
@@ -1,5 +1,5 @@
 import type { UserResolvers } from "./../../types.generated";
-export const User: Pick<UserResolvers, "fullName" | "id" | "__isTypeOf"> = {
+export const User: Pick<UserResolvers, "fullName" | "id"> = {
   fullName: ({ firstName, lastName }) => {
     return `${firstName} ${lastName}`;
   },
